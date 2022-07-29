606aa350ec06012145c0ddabbbd3b6352b535299
serhii-sultanov <sshsultanov@gmail.com>
2022-07-29 16:30:53 +0300

    created script.js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// First Task
+
+const name = prompt ("What is your name?");
+alert (name);
+console.log ("Name: ", name);
+
+// Second task
+
+let a = 3;
+let b = 4;
+
+let addition = a + b;
+let subtraction = a - b;
+let multuplication = a * b;
+let division = a / b;
+let involution = a ** b;
+
+alert (addition);
+alert (subtraction);
+alert (multuplication);
+alert (division);
+alert (involution);
+
+console.log ("a = ", a);
+console.log ("b = ", b);
+console.log ("a + b = ", addition);
+console.log ("a - b = ", subtraction);
+console.log ("a * b = ", multuplication);
+console.log ("a / b = ", division);
+console.log ("a ** b = ", involution);
